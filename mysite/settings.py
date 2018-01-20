e8d7059c10262aa7ea017ec43dc72a57d69e76f5
Xenia Kustanova <xenia.kustanova@gmail.com>
2018-01-20 14:05:10 +0100

    pridani tecky

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^3!1v_ms1#2p-_u7kok6in(lp$b+(*0w=oh0ot917(wqlre6jd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
